0292cdc17bea474c1d9001c8f9157a67a26ae198
Eduardo Vieira <bolacha0953@gmail.com>
2025-12-17 16:10:44 +0100

    fixed pointer error

--- src/main/java/net/shittytapas/fakeplayer/item/ModItems.java
+++ src/main/java/net/shittytapas/fakeplayer/item/ModItems.java
@@ -5,21 +5,28 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
 import net.minecraft.util.Identifier;
 import net.shittytapas.fakeplayer.ShittyFakePlayer;
 
 public class ModItems {
-    public static final Item FAKE_PLAYER = registerItem("fake_player", new Item(new Item.Settings()));
 
-    private static Item registerItem(String name, Item item){
-        return Registry.register(Registries.ITEM, Identifier.of(ShittyFakePlayer.MOD_ID, name), item);
-    }
+    public static final Identifier FAKE_PLAYER_ID =
+            Identifier.of(ShittyFakePlayer.MOD_ID, "fake_player");
 
-    public static void registerModItems(){
+    public static final RegistryKey<Item> FAKE_PLAYER_KEY =
+            RegistryKey.of(Registries.ITEM.getKey(), FAKE_PLAYER_ID);
+
+    public static final Item FAKE_PLAYER = Registry.register(
+            Registries.ITEM,
+            FAKE_PLAYER_ID,
+            new Item(new Item.Settings().registryKey(FAKE_PLAYER_KEY))
+    );
+
+    public static void registerModItems() {
         ShittyFakePlayer.LOGGER.info("Registering items for " + ShittyFakePlayer.MOD_ID);
 
-        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries ->{
-            entries.add(FAKE_PLAYER);
-        });
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS)
+                .register(entries -> entries.add(FAKE_PLAYER));
     }
 }
