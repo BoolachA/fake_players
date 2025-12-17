ee2613c23a06325dcb37111030313dc5410e782a
Eduardo Vieira <bolacha0953@gmail.com>
2025-12-17 15:16:34 +0100

    Initial commit

---
+++ src/main/java/net/shittytapas/fakeplayer/ShittyFakePlayer.java
@@ -0,0 +1,15 @@
+package net.shittytapas.fakeplayer;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ShittyFakePlayer implements ModInitializer {
+	public static final String MOD_ID = "shitty-fake-player";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/net/shittytapas/fakeplayer/ShittyFakePlayerClient.java
@@ -0,0 +1,10 @@
+package net.shittytapas.fakeplayer;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ShittyFakePlayerClient implements ClientModInitializer{
+    @Override
+    public void onInitializeClient(){
+
+    }
+}
